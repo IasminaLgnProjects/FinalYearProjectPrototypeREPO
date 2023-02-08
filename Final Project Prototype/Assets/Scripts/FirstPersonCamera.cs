8f1f4dc85a5177c3d7ffa7721d4e23360c0359a6
Iasmina <iasmina.leagan@gmail.com>
2023-02-08 08:33:30 -0800

    Uploading the project (fp camera + prev)

---
+++ Final Project Prototype/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    float cameraRotationVert = 0f;
+    float cameraRotationHorz = 0f;
+
+    float mouseSens = 2f;
+
+    float inputX;
+    float inputY;
+
+    // Update is called once per frame
+    void Update()
+    {      
+        //get input
+        inputX = Input.GetAxis("Mouse X") * mouseSens;
+        inputY = Input.GetAxis("Mouse Y") * mouseSens;
+
+        //make it rotate on the X axis (vertical) and Y axis (vertical)
+        cameraRotationVert -= inputY;
+        cameraRotationVert = Mathf.Clamp(cameraRotationVert, -90, 90);
+     
+        cameraRotationHorz += inputX;
+        cameraRotationHorz = Mathf.Clamp(cameraRotationHorz, -90, 90);
+
+        transform.localEulerAngles = Vector3.right * cameraRotationVert + Vector3.up * cameraRotationHorz;
+    }
+}
---
+++ Final Project Prototype/Assets/Scripts/TheGameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheGameManager : MonoBehaviour
+{
+    public GameObject ThoughtBubblePanel;
+    public GameObject ToDoListPanel;
+    public GameObject CalendarPanel;
+    public GameObject MessagePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThoughtBubblePanel.SetActive(false);
+        ToDoListPanel.SetActive(false);
+        CalendarPanel.SetActive(false);
+        MessagePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            print("1");
+            ThoughtBubblePanel.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ThoughtBubblePanel.SetActive(false);
+            CalendarPanel.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ThoughtBubblePanel.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ThoughtBubblePanel.SetActive(false);
+            CalendarPanel.SetActive(false);
+            ToDoListPanel.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            ThoughtBubblePanel.SetActive(false);
+            CalendarPanel.SetActive(false);
+            ToDoListPanel.SetActive(false);
+            MessagePanel.SetActive(true);
+        }
+    }
+}
